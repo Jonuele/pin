30af93d8c74392beb6583c02333b0a59a34a9526
Jonuele <jonathanelopez04@gmail.com>
2024-12-15 12:02:56 -0300

    Update appstramlit.py

--- appstramlit.py
+++ appstramlit.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import networkx as nx
 
 # Configuración inicial
 st.title("Aplicación de Numerología")
